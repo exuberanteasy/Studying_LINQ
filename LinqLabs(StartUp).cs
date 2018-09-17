58699cfdc30bb9dc7d5b183abe9ddb87271ca861
exuberanteasy <exuberanteasy@gmail.com>
2018-09-17 09:41:07 +0800

    Update LinqLabs(StartUp).cs

--- LinqLabs(StartUp).cs
+++ LinqLabs(StartUp).cs
@@ -1,1 +1,13 @@
 // LinqLabs (StartUp)_練習
+
+private void button4_Click(object sender, EventArgs e)
+{
+    int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    this.listBox1.Items.Clear();
+
+    foreach (int n in nums)
+    {
+        this.listBox1.Items.Add(n);
+    }
+}
