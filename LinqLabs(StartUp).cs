5911d32cc56787eee6688acbabe5cd0a4da713ec
exuberanteasy <exuberanteasy@gmail.com>
2018-09-17 10:04:59 +0800

    Update LinqLabs(StartUp).cs

--- LinqLabs(StartUp).cs
+++ LinqLabs(StartUp).cs
@@ -11,3 +11,39 @@ private void button4_Click(object sender, EventArgs e)
         this.listBox1.Items.Add(n);
     }
 }
+
+private void button4_Click(object sender, EventArgs e)
+{
+    int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    this.listBox1.Items.Clear();
+
+    foreach (int n in nums)
+    {
+        this.listBox1.Items.Add(n);
+    }
+
+    this.listBox1.Items.Add("===========================");
+
+    //??????
+   System.Collections.IEnumerator en = nums.GetEnumerator();  //IEnumerator 要注意有沒有選錯!
+
+    while (en.MoveNext())
+    {
+        this.listBox1.Items.Add(en.Current);
+    }
+}
+
+private void button5_Click(object sender, EventArgs e)
+{
+    List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 };
+    // list.Add(1)
+
+    //C# 內部轉譯
+
+    foreach (int n in list)
+    {
+        this.listBox1.Items.Add(n);
+    }
+    this.listBox1.Items.Add("===================================");
+}
